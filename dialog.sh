aa961f99042c58b2d1f0d95d7dd89a015deda4d6
Aysad Kozanoglu <k.a@tuta.io>
2016-10-22 23:45:02 +0200

    tools

---
+++ dialog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+dialog --title "HLSPANEL.COM installation Path" \
+--backtitle "HLSPANEL installation " \
+--yesno "Would you like to install hlspanel on your serve  \"/home/hlspanel/\"?" 7 60
+
+
+#  
+# Get exit status
+# 0 means user hit [yes] button.
+# 1 means user hit [no] button.
+# 255 means user hit [Esc] key.
+response=$?
+case $response in
+   0) echo "File deleted.";;
+   1) echo "File not deleted.";;
+   255) echo "[ESC] key pressed.";;
+esac
---
+++ getosinfo.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# coder: Aysad Kozanoglu
+# email : aysadx@gmail.com
+# web: http://onweb.pe.hu
+#
+
+
+lsb_release -si
+echo ""
+lsb_release -sr
+echo ""
+lsb_release -sc
+echo ""
+lsb_release -sd
