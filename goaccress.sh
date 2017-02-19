c01a87c1064eef8fab48f6cd65fbd350e81ef32a
root <root@server.takar.de>
2017-02-19 01:42:17 +0100

    Aysad Kozanoglu

---
+++ goaccress.sh
@@ -0,0 +1,2 @@
+# 0 */12 * * * /usr/local/bin/goaccess -f /usr/local/nginx/logs/access.log -o /usr/local/nginx/html/report.html --real-os -p /root/.goaccessrc
+/usr/local/bin/goaccess -f /usr/local/nginx/logs/access.log -o /usr/local/nginx/html/report.html --real-os -p /root/.goaccessrc
