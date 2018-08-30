dd8f085de993f46ef2a4f11cb16b92057036ee91
Aysad Kozanoglu <aysadx@gmail.com>
2018-08-30 22:55:15 +0200

    Create install-Nginx.sh

---
+++ conf/nginx/blesta/autoInstallparts/install-Nginx.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# author: Aysad Kozanoglu
+# email: aysadx@gmail.com
+#
+echo -e "install core packages...\n"
+apt-get -qq -y install build-essential git
+apt-get -qq -y install libpcre3 libpcre3-dev zlib1g zlib1g-dev
+cd /tmp
+wget -q http://nginx.org/download/nginx-1.12.1.tar.gz
+wget -q https://ftp.openssl.org/source/old/0.9.x/openssl-0.9.8zf.tar.gz
+tar zxvf nginx-1.12.1.tar.gz && tar zxvf openssl-0.9.8zf.tar.gz
+cd nginx-1.12.1 && ./configure --with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2' --with-ld-opt=-Wl,-z,relro --sbin-path=/usr/local/sbin --with-http_stub_status_module --with-http_ssl_module --user=www-data --group=www-data --with-openssl=/source/openssl-0.9.8zf/ --with-stream && make && make install
+echo -e "nginx php mysql installed\n"
