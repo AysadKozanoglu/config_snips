9ae8d2a82c3e3ff95ff10e4ea6b8bd5f0557bf52
Aysad Kozanoglu <aysadx@gmail.com>
2018-09-05 23:42:55 +0200

    Update installPHP7.sh

--- conf/nginx/blesta/autoInstallparts/installPHP7.sh
+++ conf/nginx/blesta/autoInstallparts/installPHP7.sh
@@ -12,6 +12,6 @@ cd /tmp
 wget https://www.dotdeb.org/dotdeb.gpg
 apt-key add dotdeb.gpg
 apt-get update
-apt-get -y install php7.0-common php7.0-fpm php7.0-cli php7.0-json php7.0-imap php7.0-mysql php7.0-curl php7.0-imap php7.0-intl php7.0-mcrypt php-pear php7.0-gd php7.0-zip php7.0-xml php7.0-mbstring
+apt-get -y install php7.0-common php7.0-fpm php7.0-cli php7.0-json php7.0-imap php7.0-mysql php7.0-curl php7.0-imap php7.0-intl php7.0-mcrypt php-pear php7.0-gd php7.0-zip php7.0-xml php7.0-mbstring php7.0-gmp
 dpkg -l | grep php7 
 /etc/init.d/php7.0-fpm start ; /etc/init.d/php7.0-fpm status
